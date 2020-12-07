9276260d983c4dfa04ef7138b4c0a424db740bce
schmid55 <schmid55@wwu.edu>
2020-12-06 19:34:15 -0800

    Day 6 complete

--- 06/1.py
+++ 06/1.py
@@ -1,2 +1,21 @@
 with open("06/in") as f:
     input = f.readlines()
+questions = {
+        "a": False, "b": False, "c": False, "d": False, "e": False, "f": False, "g": False, 
+        "h": False, "i": False, "j": False, "k": False, "l": False, "m": False, "n": False, 
+        "o": False, "p": False, "q": False, "r": False, "s": False, "t": False, "u": False, 
+        "v": False, "w": False, "x": False, "y": False, "z": False
+    }
+
+sum = 0
+for i in input:
+    if i == "\n":
+        for i in questions:
+            questions[i] = False
+    else:
+        for j in i:
+            if j != "\n" and questions[j] == False:
+                questions[j] = True
+                sum += 1
+    
+print(sum)
--- 06/2.py
+++ 06/2.py
@@ -1,2 +1,23 @@
 with open("06/in") as f:
     input = f.readlines()
+    input.append("\n")
+questions = [
+        "a", "b", "c", "d", "e", "f", "g", 
+        "h", "i", "j", "k", "l", "m", "n", 
+        "o", "p", "q", "r", "s", "t", "u", 
+        "v", "w", "x", "y", "z"
+    ]
+sum = 0
+person = []
+for i in input:
+    if i != "\n":
+        person.append(i)
+    else:
+        for k in questions:
+            yes = True
+            for j in person:
+                yes = yes and k in j
+            if yes:
+                sum += 1
+        person = []
+print(sum)
